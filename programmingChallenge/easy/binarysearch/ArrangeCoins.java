98eb9ef523acfbb7f71a5c34472d718702dfef3c
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2022-09-07 23:07:44 +0700

    add ArrangeCoins

---
+++ programmingChallenge/easy/binarysearch/ArrangeCoins.java
@@ -0,0 +1,36 @@
+package programmingChallenge.easy.binarysearch;
+
+public class ArrangeCoins {
+    // binary search approach
+    public int arrangeCoins(int n) {
+        long low = 0, high = n;
+        while(low <= high){
+            long mid = low + (high - low)/2;
+            long sum = mid * (mid +1)/2;
+
+            if(sum == n){
+                return (int) mid;
+            } else if(sum > n){
+                high = mid -1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return (int) low - 1;
+    }
+
+     public int arrangeCoins2(int n) {
+         int row = 1;
+         while(n > 0){
+             n = n - row;
+             row++;
+         }
+         return row - 1;
+     }
+
+    public static void main(String[] args){
+        ArrangeCoins arrangeCoins = new ArrangeCoins();
+        int data = arrangeCoins.arrangeCoins(5);
+        System.out.println(data);
+    }
+}
