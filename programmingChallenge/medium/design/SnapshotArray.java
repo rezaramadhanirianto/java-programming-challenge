764dcc1be0bf768f531b0f5c655b66f078692fc8
rezaramadhanirianto <rezaramadhanirianto@gmail.com>
2023-06-12 00:23:10 +0700

    add SnapshotArray

--- programmingChallenge/medium/design/SnapshotArray.java
+++ programmingChallenge/medium/design/SnapshotArray.java
@@ -1,2 +1,32 @@
-package programmingChallenge.medium.design;public class SnapshotArray {
+package programmingChallenge.medium.design;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+// https://leetcode.com/problems/snapshot-array/description/
+public class SnapshotArray {
+    List<TreeMap<Integer, Integer>> arr;
+    int currId = 0;
+
+    public SnapshotArray(int length) {
+        arr = new ArrayList();
+
+        for (int i = 0; i < length; i++) {
+            arr.add(i, new TreeMap<Integer, Integer>());
+            arr.get(i).put(0, 0);
+        }
+    }
+
+    public void set(int index, int val) {
+        arr.get(index).put(currId, val);
+    }
+
+    public int snap() {
+        return currId++;
+    }
+
+    public int get(int index, int snap_id) {
+        return arr.get(index).floorEntry(snap_id).getValue();
+    }
 }
